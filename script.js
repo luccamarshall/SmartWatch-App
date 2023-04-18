8bfd5fc5d434246df7742db8b29d4c096f2afb63
Lucca Marshall <fc58233@alunos.fc.ul.pt>
2023-04-18 14:57:55 +0100

    adicionei ficheio e uma maneira de os chamar mas ainda nao funciona

---
+++ script.js
@@ -0,0 +1,9 @@
+const buttons = document.querySelectorAll('.app button');
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        const buttonText = button.textContent.toLowerCase();
+        const fileName = buttonText.replace(/\s+/g, '');
+        window.location.href = fileName + '.html';
+    });
+});
