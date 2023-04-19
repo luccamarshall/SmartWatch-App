704b53a2d96109221c55e379192d9acc63c1b658
Lucca Marshall <fc58233@alunos.fc.ul.pt>
2023-04-19 19:52:03 +0100

    todos os botões estão a funcionar corretamente, só falta agora adicionar um botão ao offers para voltar a casa

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-window.onload = () => {
-
-    const buttons = document.querySelectorAll('.app button');
-
-    buttons.forEach(button => {
-        button.addEventListener('click', () => {
-            const buttonText = button.textContent.toLowerCase();
-            const fileName = buttonText.replace(/\s+/g, '');
-            window.location.href = fileName + '.html';
-        });
-    });
-}
-
 
 function show(clicked_id) {
 
