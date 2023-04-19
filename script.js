af965e8dcbd1ca468f3c2e59a9559acd8d8bbb3d
Lucca Marshall <fc58233@alunos.fc.ul.pt>
2023-04-19 19:00:51 +0100

    fiz o offers

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
 window.onload = () => {
 
-const buttons = document.querySelectorAll('.app button');
+    const buttons = document.querySelectorAll('.app button');
 
-buttons.forEach(button => {
-    button.addEventListener('click', () => {
-        const buttonText = button.textContent.toLowerCase();
-        const fileName = buttonText.replace(/\s+/g, '');
-        window.location.href = fileName + '.html';
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            const buttonText = button.textContent.toLowerCase();
+            const fileName = buttonText.replace(/\s+/g, '');
+            window.location.href = fileName + '.html';
+        });
     });
-});
 }
+
+
+function show(clicked_id) {
+
+    const imageId = clicked_id + '1';
+
+    const isShowing = document.getElementById(imageId).style.display === 'block';
+    
+    if (isShowing) { 
+        document.getElementById(imageId).style.display = 'none';
+    } else {
+        document.getElementById(imageId)
+            .style.display = "block";
+
+        document.getElementById(imageId)
+            .style.display = "absolute";
+    }
+}
+
+
+
+
