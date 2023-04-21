7b087b8c2453205dd1e1d407c0493dd1ac3ffc49
Lucca Marshall <fc58233@alunos.fc.ul.pt>
2023-04-21 14:10:45 +0100

    sizing and settings

--- script.js
+++ script.js
@@ -16,3 +16,36 @@ function show(clicked_id) {
     }
 }
 
+function createTextBox() {
+    const isShowingText = document.getElementById("text").style.display === 'block';
+    const isShowingRedeem = document.getElementById("redeem").style.display === 'block';
+    if (isShowingText & isShowingRedeem) {
+        document.getElementById("text").style.display = 'none';
+
+        document.getElementById("redeem").style.display = 'none';
+    } else {
+        document.getElementById("text").style.display = "block";
+
+        document.getElementById("text").style.display = "absolute";
+
+        document.getElementById("redeem").style.display = "block";
+
+        document.getElementById("redeem").style.display = "absolute";
+
+    }
+}
+
+function saveOption() {
+    var selectOption = document.getElementById("Preferences");
+    var selectedOption = selectOption.options[selectOption.selectedIndex].value;
+    localStorage.setItem("Preferences", selectedOption);
+}
+
+// Load saved option on page load
+window.onload = function () {
+    var savedOption = localStorage.getItem("Preferences");
+    if (savedOption) {
+        document.getElementById("Preferences").value = savedOption;
+    }
+};
+
