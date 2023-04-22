0b97789108ac0d6ff1e165779a524651456da1a8
Lucca Marshall <fc58233@alunos.fc.ul.pt>
2023-04-22 23:31:37 +0100

    consegui mudar as cores e guardar mas só muda a cor do settings e não afeta os ficheiros do resto do código.

--- script.js
+++ script.js
@@ -47,5 +47,49 @@ window.onload = function () {
     if (savedOption) {
         document.getElementById("Preferences").value = savedOption;
     }
+    const body = document.querySelector('#size');
+const headers = document.querySelectorAll('header');
+
+// Get saved background color and set it
+const savedBackgroundColor = localStorage.getItem('backgroundColor');
+if (savedBackgroundColor) {
+    body.style.backgroundColor = savedBackgroundColor;
+}
+
+// Get saved header color and set it
+const savedHeaderColor = localStorage.getItem('headerColor');
+if (savedHeaderColor) {
+    headers.forEach(header => {
+        header.style.color = savedHeaderColor;
+    });
+}
+
 };
+
+
+function changeColor() {
+    const headers = document.querySelectorAll('header');
+    const body = document.querySelector('#size');
+
+    if (body.style.backgroundColor === 'black') {
+        body.style.backgroundColor = 'white';
+        localStorage.setItem('backgroundColor', 'white');
+    } else {
+        body.style.backgroundColor = 'black';
+        localStorage.setItem('backgroundColor', 'black');
+    }
+
+    if (headers[0].style.color === 'white') {
+        headers.forEach(header => {
+            header.style.color = 'black';
+        });
+        localStorage.setItem('headerColor', 'black');
+    } else {
+        headers.forEach(header => {
+            header.style.color = 'white';
+        });
+        localStorage.setItem('headerColor', 'white');
+    }
+}
+
 
