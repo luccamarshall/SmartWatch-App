ed603462cf7013940c167ff1b8afc2642f517da0
RoseLysander <joanapddeus@gmail.com>
2023-04-23 11:52:58 +0100

    Supermarket.html e SuperOne.html
    Exemplo de supermarket (target, em SuperOne.html) com opções de tipos de comida (lacticíneos, carnes, etc). Necessário formatação, alguma já feita no style
    Adicionei a função display() no script

--- script.js
+++ script.js
@@ -49,3 +49,65 @@ window.onload = function () {
     }
 };
 
+//Display do SuperOne.html
+function display(){
+    var e = document.getElementById("searchOne");
+    var index = e.selectedIndex;
+    if(index==0){
+        document.getElementById("Dairy Products").style.display = 'none'
+        document.getElementById("Bakery").style.display = 'none'
+        document.getElementById("Meat").style.display = 'none'
+        document.getElementById("Fish").style.display = 'none'
+        document.getElementById("Vegetables").style.display = 'none'
+        document.getElementById("Fruits").style.display = 'none'
+    }
+    else if(index==1){
+        document.getElementById("Dairy Products").style.display = 'block'
+        document.getElementById("Bakery").style.display = 'none'
+        document.getElementById("Meat").style.display = 'none'
+        document.getElementById("Fish").style.display = 'none'
+        document.getElementById("Vegetables").style.display = 'none'
+        document.getElementById("Fruits").style.display = 'none'
+    }
+    else if(index==2){
+        document.getElementById("Dairy Products").style.display = 'none'
+        document.getElementById("Bakery").style.display = 'block'
+        document.getElementById("Meat").style.display = 'none'
+        document.getElementById("Fish").style.display = 'none'
+        document.getElementById("Vegetables").style.display = 'none'
+        document.getElementById("Fruits").style.display = 'none'
+    }
+    else if(index==3){
+        document.getElementById("Dairy Products").style.display = 'none'
+        document.getElementById("Bakery").style.display = 'none'
+        document.getElementById("Meat").style.display = 'block'
+        document.getElementById("Fish").style.display = 'none'
+        document.getElementById("Vegetables").style.display = 'none'
+        document.getElementById("Fruits").style.display = 'none'
+    }
+    else if(index==4){
+        document.getElementById("Dairy Products").style.display = 'none'
+        document.getElementById("Bakery").style.display = 'none'
+        document.getElementById("Meat").style.display = 'none'
+        document.getElementById("Fish").style.display = 'block'
+        document.getElementById("Vegetables").style.display = 'none'
+        document.getElementById("Fruits").style.display = 'none'
+    }
+    else if(index==5){
+        document.getElementById("Dairy Products").style.display = 'none'
+        document.getElementById("Bakery").style.display = 'none'
+        document.getElementById("Meat").style.display = 'none'
+        document.getElementById("Fish").style.display = 'none'
+        document.getElementById("Vegetables").style.display = 'block'
+        document.getElementById("Fruits").style.display = 'none'
+    }
+    else if(index==6){
+        document.getElementById("Dairy Products").style.display = 'none'
+        document.getElementById("Bakery").style.display = 'none'
+        document.getElementById("Meat").style.display = 'none'
+        document.getElementById("Fish").style.display = 'none'
+        document.getElementById("Vegetables").style.display = 'none'
+        document.getElementById("Fruits").style.display = 'block'
+    }
+}
+
